1860c77a4a56848d753a58436871ebc9052db256
Victor Reynolds <victor@victorreynolds.net>
2014-11-18 18:54:35 -0700

    Rearraged some of the files in the Arabic Dictionary project.

---
+++ arabic/dictionary/import_files/import_set.php
@@ -0,0 +1,32 @@
+<?php
+	//require_once($_SERVER["DOCUMENT_ROOT"] ."/inc/config.php");
+	//require_once($_SERVER["DOCUMENT_ROOT"] . "/inc/database.php");
+	require_once("lib/dictionary_manager.php");
+
+	// Importing constants
+	$importDir = "./data";
+	$completeDir = "./data_imported";
+
+	$delimTermDef = "|";
+	$delimDefTerm = "~";
+	
+	$lists = scandir($importDir);
+
+	foreach($lists as $fileName)
+	{
+		if (strpos($fileName, ".txt") !== false)
+		{
+			$filePath = $importDir . "/" . $fileName;
+
+			$data = arabicImportSet($filePath, $delimTermDef, $delimDefTerm);
+			//convertDataToHTMLEntities($data);
+			//var_dump($data);
+			formatSetForInput($data);
+			
+			importSet($data);
+			rename($filePath, $completeDir . "/" . $fileName);
+		}
+	}
+
+	//var_dump(getOrCreateSetID("poop", "fart"));
+?>
